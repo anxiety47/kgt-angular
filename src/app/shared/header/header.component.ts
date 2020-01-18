44bf35ec7e2d6992a378ebb7005dbf5d81abb42a
Kasia <anxiety477@gmail.com>
2020-01-18 14:41:33 +0100

    Added fontawesome icons and color variables

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faPaw, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  faMenuIcon = faBars;
+  faKgtIcon = faPaw;
+  faFbIcon = faFacebook;
+  faInstagramIcon = faInstagram;
 
   constructor() { }
 
