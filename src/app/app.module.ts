dc7f202a3ad310d989f0add108b60925d8b4815a
Kasia <anxiety477@gmail.com>
2020-01-18 17:48:20 +0100

    Added login-form and nav-bar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 
+const appRoutes: Routes = [
+  { path: 'login-form', component: LoginFormComponent },
+  //TODO: add default path and pageNotFoundComponent
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
+ 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginFormComponent,
+    NavBarComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule
