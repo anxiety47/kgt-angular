eba584149217843ae041af66e00bf48b5b45a732
Kasia <anxiety477@gmail.com>
2020-01-27 23:50:54 +0100

    Added routing to login-form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginFormComponent } from './core/authentication/login-form/login-form.component';
+import { HomeComponent } from './modules/home/home/home.component';
 
-
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: 'login-form', component: LoginFormComponent },
+  { path: '', component: HomeComponent }
+  // TODO: add default path and pageNotFoundComponent
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { Routes, RouterModule } from '@angular/router';
 import { LoginFormComponent } from './core/authentication/login-form/login-form.component';
 import { SideNavBarComponent } from './core/side-nav-bar/side-nav-bar.component';
 import { HomeComponent } from './modules/home/home/home.component';
-
-const appRoutes: Routes = [
-  { path: 'login-form', component: LoginFormComponent },
-  // TODO: add default path and pageNotFoundComponent
-  // { path: 'hero/:id',      component: HeroDetailComponent },
-  // { path: '',
-  //   redirectTo: '/heroes',
-  //   pathMatch: 'full'
-  // },
-  // { path: '**', component: PageNotFoundComponent }
-];
 
 // TODO: use core/shared/module ***.module.ts files
 @NgModule({
@@ -31,10 +19,6 @@ const appRoutes: Routes = [
     HomeComponent
   ],
   imports: [
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    ),
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule
