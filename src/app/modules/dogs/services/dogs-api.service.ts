645e5821fb8fb35e212d70b4641a189af327aca7
Kasia <anxiety477@gmail.com>
2021-01-28 21:49:58 +0100

    Add dogs list mock data

--- src/app/modules/dogs/services/dogs-api.service.ts
+++ src/app/modules/dogs/services/dogs-api.service.ts
@@ -1,14 +1,14 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Dog, DogLevel } from "@app/shared/models/dog";
-import { Observable, of } from "rxjs";
+import { Dog } from "@app/shared/models/dog";
+import { environment } from "@environments/environment";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class DogsApiService {
   constructor(private httpClient: HttpClient) {}
 
   public getAllDogs(): Observable<Dog[]> {
-    // return this.httpClient.get<Dog[]>('TODO: ADRES_API')
-    return of([{id: 1, name: 'Okruszek', breed: 'Owczarek niemiecki', dateOfBirth: new Date(), level: DogLevel.Expert, workmode: '', notes: 'notatki1'}]);
+    return this.httpClient.get<Dog[]>(`${environment.apiUrl}/dogs`);
   }
 }
