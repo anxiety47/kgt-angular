2f0c9cb773709e3243b7a2364e0b9b7bc8bed8ac
Kasia <anxiety477@gmail.com>
2021-02-03 22:57:28 +0100

    add UTs for dogs list component

--- src/app/modules/dogs/components/dogs-list/dogs-list.component.spec.ts
+++ src/app/modules/dogs/components/dogs-list/dogs-list.component.spec.ts
@@ -1,25 +1,56 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
+import { Component, Injectable, Input } from '@angular/core';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Dog } from '@app/shared/models/dog';
+import { Observable, of } from 'rxjs';
+import { DogsService } from '../../services/dogs.service';
 import { DogsComponent } from './dogs-list.component';
+
+// TODO: move mocks to separate files
+@Injectable()
+class DogsServiceMock {
+  dogs$: Observable<Dog[]> = of();
+
+  public getAllDogs(): void {}
+}
+
+@Component({
+  selector: 'app-dogs-list-card',
+  template: '<p>Mock for DogsListCardComponent</p>'
+})
+class DogsListCardComponentMock {
+  @Input() dog: Dog;
+}
 
 describe('DogsComponent', () => {
   let component: DogsComponent;
   let fixture: ComponentFixture<DogsComponent>;
+  let dogsService: DogsService;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ DogsComponent ]
+      declarations: [ DogsComponent, DogsListCardComponentMock ],
+      providers: [ 
+        { provide: DogsService, useClass: DogsServiceMock }
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DogsComponent);
+    dogsService = TestBed.get(DogsService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call function from service that gets all dogs', () => {
+    const getAllDogsSpy = spyOn(dogsService, 'getAllDogs');
+    component.ngOnInit();
+    expect(getAllDogsSpy).toHaveBeenCalled();
   });
 });
