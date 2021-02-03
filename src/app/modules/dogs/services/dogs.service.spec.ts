957a07df8bee6cf806722886981914f102e98705
Kasia <anxiety477@gmail.com>
2021-02-03 22:57:07 +0100

    add UTs for dogs service

--- src/app/modules/dogs/services/dogs.service.spec.ts
+++ src/app/modules/dogs/services/dogs.service.spec.ts
@@ -1,12 +1,29 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Store, StoreModule } from '@ngrx/store';
+import { GetAllDogs } from '../store/actions';
 import { DogsService } from './dogs.service';
 
 describe('DogsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let store: Store<any>;
+  let dogsService: DogsService;
+  
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [StoreModule.forRoot({})],
+    });
+
+    store = TestBed.get(Store);
+    dogsService = TestBed.get(DogsService);
+  });
 
   it('should be created', () => {
     const service: DogsService = TestBed.get(DogsService);
     expect(service).toBeTruthy();
+  });
+
+  it('should dispatch action that gets all dogs data', () => {
+    let tmp = spyOn(store, 'dispatch');
+    dogsService.getAllDogs();
+    expect(tmp).toHaveBeenCalledWith( new GetAllDogs() );
   });
 });
